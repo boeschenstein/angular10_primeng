1740f3f24918d556bd0dd9c0050b99dd37df32fd
Boeschenstein, Patrik <patrik.boeschenstein@hotmail.com>
2020-09-20 16:06:41 +0200

    FlexBox: FormLayout example

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agile-times';
+
+  selectedState: City = null;
+
+  states: City[] = [
+      {name: 'Arizona', code: 'Arizona'},
+      {name: 'California', value: 'California'},
+      {name: 'Florida', code: 'Florida'},
+      {name: 'Ohio', code: 'Ohio'},
+      {name: 'Washington', code: 'Washington'}
+  ];
+
+  cities1: City[] = [];
+
+  cities2: City[] = [];
+
+  city1: string|null = null;
+
+  city2: string|null = null;
+
+}
+
+interface City {
+  name: string;
+  code?: string|null;
+  value?: string|null;
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +16,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ButtonModule
+    ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    BrowserAnimationsModule,
+    InputTextareaModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
